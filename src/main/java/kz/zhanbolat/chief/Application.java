8de45e8f601ccb8b2146278a3b7985ba27e8febc
Zhanbolat Orakbayev <fous363@gmail.com>
2020-09-27 00:02:35 +0600

    Use reflection to create objects

--- src/main/java/kz/zhanbolat/chief/Application.java
+++ src/main/java/kz/zhanbolat/chief/Application.java
@@ -13,6 +13,8 @@ import kz.zhanbolat.chief.service.finder.impl.IngredientFinderImpl;
 import kz.zhanbolat.chief.service.impl.ChiefServiceImpl;
 import kz.zhanbolat.chief.service.sorter.VegetableSorter;
 import kz.zhanbolat.chief.service.sorter.impl.VegetableSorterImpl;
+import kz.zhanbolat.chief.util.ReflectionCreator;
+import kz.zhanbolat.chief.util.impl.ReflectionCreatorImpl;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,9 +23,10 @@ import java.util.List;
     add handler on ingredient's endness, think about adding the takeout for foods
 */
 public class Application {
+    private static final ReflectionCreator reflectionCreator = new ReflectionCreatorImpl();
 
     public static void main(String[] args) {
-        ChiefService chiefService = new ChiefServiceImpl();
+        ChiefService chiefService = reflectionCreator.instantiate(ChiefServiceImpl.class);
         Dish greekSalad = chiefService.cookDish(DishType.GREEK_SALAD);
         System.out.println("Greek salad:");
         for (Ingredient ingredient : greekSalad.getCookedIngredients()) {
@@ -31,9 +34,9 @@ public class Application {
         }
 
         System.out.println("Total calories: " + greekSalad.getCalories() + " gram");
-        IngredientFilter vegetableFilter = new VegetableIngredientFilter();
+        IngredientFilter vegetableFilter = reflectionCreator.instantiate(VegetableIngredientFilter.class);
         List<Ingredient> greekSaladVegetables = vegetableFilter.filterIngredients(greekSalad.getCookedIngredients());
-        VegetableSorter vegetableSorter = new VegetableSorterImpl();
+        VegetableSorter vegetableSorter = reflectionCreator.instantiate(VegetableSorterImpl.class);
         List<OrganicIngredient> sortedVegetables = vegetableSorter.sortByWeightAsc(greekSaladVegetables);
         System.out.println("Sorted vegetables: " + Arrays.toString(sortedVegetables.toArray()));
 
@@ -44,7 +47,7 @@ public class Application {
         }
         System.out.println("Total calories: " + borsch.getCalories() + " gram");
 
-        IngredientFinder ingredientFinder = new IngredientFinderImpl();
+        IngredientFinder ingredientFinder = reflectionCreator.instantiate(IngredientFinderImpl.class);
         SearchParams params = SearchParams.builder().setBoiled(true).setMinWeight(200).setMaxWeight(500).build();
         Ingredient ingredient = ingredientFinder.findIngredient(params, borsch.getCookedIngredients());
         System.out.println("Found: " + ingredient);
