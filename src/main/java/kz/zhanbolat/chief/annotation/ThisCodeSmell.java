106c6ec3208d594a9f231c0bdf7e11cf8b3d8808
Zhanbolat Orakbayev <fous363@gmail.com>
2020-09-28 23:14:55 +0600

    Fix issue with not reading via reflection api

--- src/main/java/kz/zhanbolat/chief/annotation/ThisCodeSmell.java
+++ src/main/java/kz/zhanbolat/chief/annotation/ThisCodeSmell.java
@@ -2,14 +2,14 @@ package kz.zhanbolat.chief.annotation;
 
 import java.lang.annotation.*;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.FIELD,
         ElementType.METHOD, ElementType.TYPE})
 @Repeatable(ThisCodeSmell.List.class)
 public @interface ThisCodeSmell {
     String reviewer();
 
-    @Retention(RetentionPolicy.SOURCE)
+    @Retention(RetentionPolicy.RUNTIME)
     @Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.FIELD,
             ElementType.METHOD, ElementType.TYPE})
     @interface List {
