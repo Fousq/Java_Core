561c2220be842d6ccdc4a0aa0cd8be9a4c284e62
Zhanbolat Orakbayev <fous363@gmail.com>
2020-09-29 01:03:26 +0600

    Impl ThisCodeSmell annotation handler

--- src/main/java/kz/zhanbolat/chief/Application.java
+++ src/main/java/kz/zhanbolat/chief/Application.java
@@ -1,6 +1,8 @@
 package kz.zhanbolat.chief;
 
 import kz.zhanbolat.chief.annotation.ThisCodeSmell;
+import kz.zhanbolat.chief.annotation.handler.ThisCodeSmellAnnotationHandler;
+import kz.zhanbolat.chief.annotation.handler.impl.ThisCodeSmellAnnotationHandlerImpl;
 import kz.zhanbolat.chief.entity.Ingredient;
 import kz.zhanbolat.chief.entity.dish.Dish;
 import kz.zhanbolat.chief.entity.ingredient.organic.OrganicIngredient;
@@ -14,15 +16,31 @@ import kz.zhanbolat.chief.service.finder.impl.IngredientFinderImpl;
 import kz.zhanbolat.chief.service.impl.ChiefServiceImpl;
 import kz.zhanbolat.chief.service.sorter.VegetableSorter;
 import kz.zhanbolat.chief.service.sorter.impl.VegetableSorterImpl;
+import kz.zhanbolat.chief.util.ReflectionScanner;
+import kz.zhanbolat.chief.util.ReflectionScannerImpl;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+@ThisCodeSmell(reviewer = "Zhanbolat")
 public class Application {
 
     @ThisCodeSmell(reviewer = "Zhanbolat")
     @ThisCodeSmell(reviewer = "Another reviewer")
     public static void main(String[] args) {
+        ReflectionScanner scanner = new ReflectionScannerImpl();
+        Class<?>[] classes = new Class[0];
+        try {
+             classes = scanner.getClasses(Application.class.getPackage().getName());
+        } catch (IOException e) {
+            System.err.println("Failed to get classes");
+        }
+        ThisCodeSmellAnnotationHandler<String, Integer> thisCodeSmellAnnotationHandler = new ThisCodeSmellAnnotationHandlerImpl();
+        System.out.println("The start of ThisCodeSmell annotation handler:");
+        thisCodeSmellAnnotationHandler.handle(classes);
+        System.out.println("The end of ThisCodeSmell annotation handler.");
+
         ChiefService chiefService = new ChiefServiceImpl();
         Dish greekSalad = chiefService.cookDish(DishType.GREEK_SALAD);
         System.out.println("Greek salad:");
@@ -48,5 +66,7 @@ public class Application {
         SearchParams params = SearchParams.builder().setBoiled(true).setMinWeight(200).setMaxWeight(500).build();
         Ingredient ingredient = ingredientFinder.findIngredient(params, borsch.getCookedIngredients());
         System.out.println("Found: " + ingredient);
+
+        System.out.println(thisCodeSmellAnnotationHandler.getCodeAreaReviewersSortedMap());
     }
 }
