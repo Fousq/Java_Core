2247039d8bcdf1ad7f79a80fb8b5ccad246e96e5
Zhanbolat Orakbayev <fous363@gmail.com>
2020-09-28 23:13:45 +0600

    Impl scanner for classes

---
+++ src/main/java/kz/zhanbolat/chief/util/ReflectionScannerImpl.java
@@ -0,0 +1,50 @@
+package kz.zhanbolat.chief.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class ReflectionScannerImpl implements ReflectionScanner {
+    @Override
+    public Class<?>[] getClasses(String packageName) throws IOException {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList<>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        List<Class<?>> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        return classes.toArray(new Class[0]);
+    }
+
+    private List<Class<?>> findClasses(File directory, String packageName) {
+        List<Class<?>> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory() && !file.getName().contains(".")) {
+                    classes.addAll(findClasses(file, packageName + "." + file.getName()));
+                } else if (file.getName().endsWith(".class")) {
+                    String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                    try {
+                        classes.add(Class.forName(className));
+                    } catch (ClassNotFoundException e) {
+                        System.err.println("No such class with name " + className);
+                    }
+                }
+            }
+        }
+        return classes;
+    }
+}
